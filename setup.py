1e1aae43507709bfc4f34409575c4f06e70d8d23
mapoferri <mariapaola.ferri@studio.unibo.it>
2025-03-27 12:01:59 +0100

    core: changing docs for radiospineomics

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="biobb_morph",
+    name="biobb_radiospineomicsw",
     version="5.0.2",
     author="Biobb developers",
     author_email="mferri@bsc.es",
-    description="biobb_morph is the Biobb module collection to create patien-specific 3D meshes from IVD template examples.",
+    description="biobb_radiospineomics.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="Bioinformatics Workflows BioExcel Compatibility",
-    url="https://github.com/bioexcel/biobb_morph",
+    url="https://github.com/bioexcel/biobb_radiospineomics",
     project_urls={
-        "Documentation": "http://biobb-morph.readthedocs.io/en/latest/",
+        "Documentation": "http://biobb-radiospineomics.readthedocs.io/en/latest/",
         "Bioexcel": "https://bioexcel.eu/",
     },
     packages=setuptools.find_packages(exclude=["docs", "test"]),
-    package_data={"biobb_morph": ["py.typed"]},
+    package_data={"biobb_radiospineomics": ["py.typed"]},
     include_package_data=True,
     install_requires=[
         "biobb_common==5.0.0",
@@ -33,7 +33,7 @@ setuptools.setup(
         "Rtree",
     ],
     python_requires=">=3.9",
-    entry_points={"console_scripts": ["morph = biobb_morph.morph.morph:main"]},
+    entry_points={"console_scripts": ["radiospineomics = biobb_radiospineomics.radiospineomics.radiospineomics:main"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3.9",
